894fd41654f8024d0351d45caf48d0726c8dadd7
abhishek-888 <54302231+abhishek-888@users.noreply.github.com>
2022-11-02 17:23:47 +0530

    Initial commit

---
+++ src/test/java/com/Barclays/Account_Managment/AccountManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barclays.Account_Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
